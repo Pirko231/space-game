e53bd05d6c492436e5edba68bd7ab87488167990
Pirko231 <szymek.pirowski@gmail.com>
2025-04-18 18:33:10 +0200

    asteroidy mogą pojawiac sie na koordynatach ujemnych. Asteroidy mogą miec rozne kierunki obracania sie i poruszania.

--- src/game/gameObjects/factories/asteroidFactory.hpp
+++ src/game/gameObjects/factories/asteroidFactory.hpp
@@ -5,11 +5,8 @@
 class AsteroidFactory
 {
 public:
-    std::unique_ptr<Asteroid> create(sf::Vector2f _pos)
+    std::unique_ptr<Asteroid> create(sf::Vector2f _pos, sf::Vector2f moveBy)
     {
-        sf::Vector2f moveBy{static_cast<float>(std::rand() % 20), static_cast<float>(std::rand() % 20)};
-        if (moveBy.x == 0 && moveBy.y == 0)
-            moveBy += {0.1,0.1};
-        return std::make_unique<Asteroid>(Asteroid{_pos, moveBy.normalized(), util::AssetLoader::get().asteroids[0]});
+        return std::make_unique<Asteroid>(Asteroid{_pos, moveBy, util::AssetLoader::get().asteroids[0]});
     }
 };
