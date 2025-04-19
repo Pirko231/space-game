ff63cfb778cb4b7977caa4b8facbcda890ba505f
Pirko231 <szymek.pirowski@gmail.com>
2025-04-19 12:50:32 +0200

    nowy algorytm do generowania mapy

--- src/game/playerUI/map.hpp
+++ src/game/playerUI/map.hpp
@@ -5,20 +5,27 @@
 class Map : public sf::Drawable
 {
 public:
-    Map(int amount, sf::Vector2f scale = {4.f,4.f})
+    Map(int rectSize, sf::Vector2f scale = {4.f,4.f})
     {
         sf::Vector2f startPos{};
-        for (std::size_t i = 0; i < amount; i++)
+        sf::Sprite forCopy{util::AssetLoader::get().background};
+        forCopy.setScale(scale);
+        for (int i = 0; i < rectSize; i++)
         {
-            sf::Sprite sprite{util::AssetLoader::get().background};
-            sprite.setScale(scale);
-            sprite.setPosition(startPos);
-            bounds.size.x += sprite.getGlobalBounds().size.x;
-            bounds.size.y += sprite.getGlobalBounds().size.y;
-            startPos.x += sprite.getGlobalBounds().size.x;
-            sprites.push_back(std::move(sprite));
+            for (int j = 0; j < rectSize; j++)
+            {
+                sf::Sprite sprite{forCopy};
+                sprite.setPosition(startPos);
+                startPos.x += forCopy.getGlobalBounds().size.x;
+                sprites.push_back(std::move(sprite));
+            }
+            startPos.y += forCopy.getGlobalBounds().size.y;
         }
+        bounds.size.x = forCopy.getGlobalBounds().size.x * rectSize;
+        bounds.size.y = forCopy.getGlobalBounds().size.y * rectSize;
     }
+
+    sf::FloatRect getBounds() const {return bounds;}
 private:
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override
     {
