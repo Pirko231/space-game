a392806e5169bddb711c50b2202bdab84316c122
Pirko231 <szymek.pirowski@gmail.com>
2025-04-20 14:02:21 +0200

    rysowanie obiektow na ekran w pewnym zasiegu od gracza. Na razie za duzo kopii

--- src/game/playerUI/radar.cpp
+++ src/game/playerUI/radar.cpp
@@ -7,4 +7,32 @@ Radar::Radar() : sprite{util::AssetLoader::get().radar}
 
 void Radar::update()
 {
+    findTargets(p1Hitbox->getCenter());
+    removeTargets(p1Hitbox->getCenter());
+}
+
+void Radar::findTargets(sf::Vector2f playerPos)
+{
+    sf::FloatRect hitbox{{playerPos.x - range / 2.f, playerPos.y - range / 2.f}, {range, range}};
+    for (auto& i : asteroids->getAsteroids())
+        if (hitbox.findIntersection(i->getGlobalBounds()))
+        {
+            sf::RectangleShape shape{{5.f,5.f}};
+            shape.setPosition(i->getGlobalBounds().position);
+            currentlyDisplayed.push_back({std::move(shape), i->getGlobalBounds().position});
+        }
+}
+
+void Radar::removeTargets(sf::Vector2f playerPos)
+{
+    sf::FloatRect hitbox{{playerPos.x - range / 2.f, playerPos.y - range / 2.f}, {range, range}};
+    /*for (auto& [shape, pos] : currentlyDisplayed)
+        if (!hitbox.contains(pos))*/
+    for (std::size_t i = 0; i < currentlyDisplayed.size(); i++)
+        if (!hitbox.contains(currentlyDisplayed[i].second))
+        {
+            currentlyDisplayed.erase(currentlyDisplayed.begin() + i);
+            i--;
+        }
+            
 }
--- src/game/playerUI/radar.hpp
+++ src/game/playerUI/radar.hpp
@@ -15,9 +15,19 @@ public:
 
     sf::FloatRect getGlobalBounds() const {return sprite.getGlobalBounds();}
 private:
+    void findTargets(sf::Vector2f playerPos);
+    void removeTargets(sf::Vector2f playerPos);
+
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override
     {
         target.draw(sprite, states);
+        for (auto& i : currentlyDisplayed)
+            target.draw(i.first, states);
     }
     sf::Sprite sprite;
+    float range{100.f};
+    /// @brief para obiektow
+    /// @param sf::RectangleShape - obiekt na radarze
+    /// @param sf::Vector2f pozycja obiektu bez przeskalowania na koordynaty radaru
+    std::vector<std::pair<sf::RectangleShape, sf::Vector2f>> currentlyDisplayed;
 };
