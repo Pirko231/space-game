b44673f21317fc0e34188722251dbb7225a3cb01
Pirko231 <szymek.pirowski@gmail.com>
2025-04-20 17:43:50 +0200

    przeniesienie obiektow na radar

--- src/game/playerUI/radar.hpp
+++ src/game/playerUI/radar.hpp
@@ -2,6 +2,8 @@
 #include "managers/collisionManager.hpp"
 #include "assetLoader.hpp"
 
+
+
 class Radar : private CollisionManager, public sf::Drawable
 {
 public:
@@ -17,21 +19,25 @@ public:
 private:
     void findTargets(sf::Vector2f playerPos);
     void removeTargets(sf::Vector2f playerPos);
-    void convertCoordinates();
+    void convertCoordinates(sf::Vector2f playerPos);
 
     bool isRepeated(const std::unique_ptr<Asteroid>*);
-    void moveTargets();
 
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override
     {
         target.draw(sprite, states);
         for (auto& i : currentlyDisplayed)
             target.draw(i.first, states);
+        target.draw(p1, states);
     }
     sf::Sprite sprite;
     float range{500.f};
+    float scale{};
     /// @brief para obiektow
     /// @param sf::RectangleShape - obiekt na radarze
     /// @param std::unique_ptr<Asteroid>* wksaznik do orginalnego obiektu
     std::vector<std::pair<sf::RectangleShape, const std::unique_ptr<Asteroid>*>> currentlyDisplayed;
+
+    sf::RectangleShape p1;
+    sf::RectangleShape p2;
 };
