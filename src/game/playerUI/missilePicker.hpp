8b6547984d4c6e743c8748eab54efc2591d5fab7
Pirko231 <szymek.pirowski@gmail.com>
2025-04-22 16:36:08 +0200

    dodanie grafik do wyboru broni

--- src/game/playerUI/missilePicker.hpp
+++ src/game/playerUI/missilePicker.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <span>
+#include "assetLoader.hpp"
 #include "missileFactory.hpp"
 #include "factories/laserFactory.hpp"
 #include "factories/rocketFactory.hpp"
@@ -8,7 +9,8 @@
 
 struct Background : public sf::Drawable
 {
-    Background(int amount) : spritesArr{new sf::RectangleShape[amount]}, sprites{spritesArr, amount}
+    Background(int amount)
+    : spritesArr{new sf::RectangleShape[amount]}, sprites{spritesArr, amount}, missiles{missileTextures}
     {
         
         for (int i = 0; i < sprites.size(); i++)
@@ -18,6 +20,11 @@ struct Background : public sf::Drawable
             sprites[i].setFillColor(sf::Color{0,0,0,0});
             sprites[i].setSize({10.f,10.f});
         }
+        for (auto& i : missiles)
+            i.setOrigin(i.getGlobalBounds().getCenter());
+        missiles[0].setScale({0.2f,0.2f});
+        missiles[1].setScale({0.2f,0.2f});
+        missiles[2].setScale({0.2f,0.2f});
     }
     void setPosition(sf::Vector2f pos)
     {
@@ -25,6 +32,11 @@ struct Background : public sf::Drawable
         {
             i.setPosition(pos);
             pos.x += i.getGlobalBounds().size.x;
+        }
+        //pos.x -= 3 * sprites.begin()->getGlobalBounds().size.x;
+        for (std::size_t i = 0; i < missiles.size(); i++)
+        {
+            missiles[i].setPosition(sprites[i].getGlobalBounds().getCenter());
         }
     }
     sf::Vector2f getPosition() const {return sprites.begin()->getPosition();}
@@ -34,10 +46,14 @@ private:
     {
         for (auto& i : sprites)
             target.draw(i, states);
+        for (auto& i : missiles)
+            target.draw(i, states);
     }
    
     sf::RectangleShape* spritesArr{};
     std::span<sf::RectangleShape>sprites;
+    sf::Sprite missileTextures[3] {sf::Sprite{util::AssetLoader::get().laser}, sf::Sprite{util::AssetLoader::get().rocket}, sf::Sprite{util::AssetLoader::get().scanner}};
+    std::span<sf::Sprite>missiles;
 };
 
 class MissilePicker : public sf::Drawable
