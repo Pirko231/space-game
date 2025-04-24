88b26a24f34c526b47959a6c98ad6df18ce9ae90
Pirko231 <szymek.pirowski@gmail.com>
2025-04-24 19:08:27 +0200

    dokumentacja

--- docs/html/search/all_8.js
+++ docs/html/search/all_8.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['map_0',['Map',['../classMap.html',1,'']]],
   ['missile_1',['Missile',['../classMissile.html',1,'']]],
-  ['missilemanager_2',['MissileManager',['../classMissileManager.html',1,'']]]
+  ['missilemanager_2',['MissileManager',['../classMissileManager.html',1,'']]],
+  ['missilepicker_3',['MissilePicker',['../structMissilePicker.html',1,'']]]
 ];
