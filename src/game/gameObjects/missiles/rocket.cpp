6149b51e1b26b690241127d3151baa7fea060fa4
Pirko231 <szymek.pirowski@gmail.com>
2025-04-26 11:52:58 +0200

    praca nad rakieta (skala ruch i obrazenia)

--- src/game/gameObjects/missiles/rocket.cpp
+++ src/game/gameObjects/missiles/rocket.cpp
@@ -4,6 +4,23 @@ void Rocket::init()
 {
 }
 
+void Rocket::handleEvents(const std::optional<sf::Event>& ev)
+{
+    if (const auto* keyReleased = ev->getIf<sf::Event::KeyPressed>())
+    {
+        if (keyReleased->code == sf::Keyboard::Key::A)
+        {
+            sprite.rotate(-rotationSpeed);
+            moveBy = moveBy.rotatedBy(-rotationSpeed);
+        }
+        if (keyReleased->code == sf::Keyboard::Key::D)
+        {
+            sprite.rotate(rotationSpeed);
+            moveBy = moveBy.rotatedBy(rotationSpeed);
+        }
+    }
+}
+
 void Rocket::update()
 {
     sprite.move(moveBy);
--- src/game/gameObjects/missiles/rocket.hpp
+++ src/game/gameObjects/missiles/rocket.hpp
@@ -5,15 +5,18 @@ class Rocket : public Missile
 {
 public:
     Rocket(sf::Vector2f pos, sf::Vector2f moveBy)
-    : Missile{50.f, 1.f, 50, util::AssetLoader::get().rocket} 
+    : Missile{800.f, 1.f, 50, util::AssetLoader::get().rocket} 
     {
+        damage = 50;
+        sprite.setScale({0.4f,0.4f});
         launch(pos,moveBy);
     }
     static bool shouldInit() {static bool initted{false}; if (!initted) {initted = true;  return true;} return false;}
     static void init();
 
-    virtual void update();
-    virtual void display();
+    void handleEvents(const std::optional<sf::Event>& ev) override;
+    void update();
+    void display();
 private:
-
+    sf::Angle rotationSpeed{sf::degrees(0.5f)};
 };
