9c26c591bd63711eb271a0bb32b27e3b83b9f2fe
Pirko231 <szymek.pirowski@gmail.com>
2025-04-28 18:35:45 +0200

    dodanie miny

--- src/game/gameObjects/missiles/mine.hpp
+++ src/game/gameObjects/missiles/mine.hpp
@@ -5,8 +5,9 @@ class Mine : public Missile
 {
 public:
     Mine(sf::Vector2f pos, sf::Vector2f moveBy)
-    : Missile{50.f, 1.f, 0, util::AssetLoader::get().mine}
+    : Missile{1000.f, 0.f, 50, util::AssetLoader::get().mine}
     {
+        sprite.setScale({0.2f,0.2f});
         launch(pos,moveBy);
     }
     static bool shouldInit() {static bool initted{false}; if (!initted) {initted = true;  return true;} return false;}
