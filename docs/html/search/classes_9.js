32e08c59ed92634f0462c5cc3939cb011f710496
Pirko231 <szymek.pirowski@gmail.com>
2025-04-29 16:44:49 +0200

    dokumentacja

--- docs/html/search/classes_9.js
+++ docs/html/search/classes_9.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['scanner_0',['Scanner',['../classScanner.html',1,'']]],
-  ['scannerfactory_1',['ScannerFactory',['../classScannerFactory.html',1,'']]],
-  ['shield_2',['Shield',['../classShield.html',1,'']]],
-  ['speedtracker_3',['SpeedTracker',['../classperf_1_1SpeedTracker.html',1,'perf']]]
+  ['shield_0',['Shield',['../classShield.html',1,'']]],
+  ['speedtracker_1',['SpeedTracker',['../classperf_1_1SpeedTracker.html',1,'perf']]]
 ];
